65ca950610115437acf9bbe4ae2ca337e9b4832a
Dominik Buszewski <yogiasdasd@gmail.com>
2020-04-03 17:12:52 +0200

    adding collection overview page, moved shop data into its own reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,26 +1,13 @@
 import React from "react";
-import Shop_Data from "./shop.data.js";
-import CollectionPreview from "../../components/preview-collection/collection-preview.component";
+import { Route } from "react-router-dom";
+import CollectionPage from "../collection/collection.component";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: Shop_Data
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = ({ match }) => (
+  <div className="shop-page">
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
 
 export default ShopPage;
