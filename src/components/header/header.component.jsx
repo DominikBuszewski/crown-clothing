49047aa2935adf8b47a52f9cadd1d2ba1a17fad9
Dominik Buszewski <yogiasdasd@gmail.com>
2020-05-11 07:56:04 +0200

    deleting empty contact page

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,41 +9,38 @@ import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 import { createStructuredSelector } from "reselect";
 import { selectCurrentUser } from "../../redux/user/user.selectors";
 import {
-  selectCartHidden
-  // selectCartItems
+	selectCartHidden,
+	// selectCartItems
 } from "../../redux/cart/cart.selectors";
 
 const Header = ({ currentUser, hidden }) => (
-  <div className="header">
-    <Link className="logo-container" to="/">
-      <Logo className="logo" />
-      <p className="logo-title">Crown Shop</p>
-    </Link>
-    <div className="options">
-      <Link className="option" to="/shop">
-        SHOP
-      </Link>
-      <Link className="option" to="/contact">
-        CONTACT
-      </Link>
-      {currentUser ? (
-        <div className="option" onClick={() => auth.signOut()}>
-          SIGN OUT
-        </div>
-      ) : (
-        <Link className="option" to="/signin">
-          SIGN IN
-        </Link>
-      )}
-      <CartIcon />
-    </div>
-    {hidden ? null : <CartDropdown />}
-  </div>
+	<div className="header">
+		<Link className="logo-container" to="/">
+			<Logo className="logo" />
+			<p className="logo-title">Crown Shop</p>
+		</Link>
+		<div className="options">
+			<Link className="option" to="/shop">
+				SHOP
+			</Link>
+			{currentUser ? (
+				<div className="option" onClick={() => auth.signOut()}>
+					SIGN OUT
+				</div>
+			) : (
+				<Link className="option" to="/signin">
+					SIGN IN
+				</Link>
+			)}
+			<CartIcon />
+		</div>
+		{hidden ? null : <CartDropdown />}
+	</div>
 );
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser,
-  hidden: selectCartHidden
+	currentUser: selectCurrentUser,
+	hidden: selectCartHidden,
 });
 
 export default connect(mapStateToProps)(Header);
